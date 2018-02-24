3c02708ca22c4d23baaa97ebea09eb6082959655
Terence Huynh <me@terencehuynh.com>
2018-02-24 17:17:50 +1100

    Cleanup

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react'
-import { SearchBar } from '../SearchBar/searchBar'
+import { SearchBar } from '../SearchBar'
 
 import './style.css'
-
-const homepageHeader = (
-  <header className="app-header app-header-homepage">
-    <div className="container">
-      <h1 className="app-title">LegalEagle</h1>
-      <SearchBar />
-    </div>
-  </header>
-)
-
-const pageHeader = (
-  <header className="app-header">
-    <div className="container">
-      <h1 className="app-title">LegalEagle</h1>
-      <SearchBar />
-    </div>
-  </header>
-)
 
 export class AppHeader extends Component {
   render() {
     const { homepage } = this.props
-    return homepage ? homepageHeader : pageHeader
+    const className = homepage ? 'app-header app-header-homepage' : 'app-header'
+    return (
+      <header className={className}>
+        <div className="container">
+          <h1 className="app-title">LegalEagle</h1>
+          <SearchBar />
+        </div>
+      </header>
+    )
   }
 }
